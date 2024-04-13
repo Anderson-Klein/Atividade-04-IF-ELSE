0b44fa8eeb0bc3f926ec40b5b0104bb7eaeb0293
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-04-12 23:57:29 -0300

    Update Atividade-4-3_Verificação de login.java

--- Atividade-4-3_Verificação de login.java
+++ Atividade-4-3_Verificação de login.java
@@ -8,16 +8,16 @@ public class main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Nome do usuario: ");
+        System.out.println("Nome do usuario: ");
         nome = scanner.nextLine();
 
-        System.out.printf("Senha do usuario: ");
+        System.out.println("Senha do usuario: ");
         senha = scanner.nextLine();
 
        if (nome.equals("usuario") && senha.equals("senha123")) {
-           System.out.printf("Login bem-sucedido");
+           System.out.println("Login bem-sucedido");
        }else {
-           System.out.printf("Nome de usuário ou senha incorretos");
+           System.out.println("Nome de usuário ou senha incorretos");
        }
     }
 }
